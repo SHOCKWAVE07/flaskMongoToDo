6b176bc061ea7af6044caa91464f2592bca1e87c
Varun <varunraskar22@gmail.com>
2023-07-08 22:27:58 +0530

    deployment

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
 app.config["SECRET_KEY"] = 'a8bbb89011cc826c2bccb6fc1f53d779c1e59814'
 app.config["MONGO_URI"]='mongodb+srv://varunraskar22:Shockwave07@cluster0.4tcbg1f.mongodb.net/todo'
 
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
 
-
-from application import routes
