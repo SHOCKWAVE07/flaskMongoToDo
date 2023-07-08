17fbd7bbd73946d1a3364f86659a57a935aed34f
Varun <varunraskar22@gmail.com>
2023-07-08 23:08:51 +0530

    sample

--- main/__init__.py
+++ main/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 application = Flask(__name__)
-application.config["SECRET_KEY"] = 'a8bbb89011cc826c2bccb6fc1f53d779c1e59814'
-application.config["MONGO_URI"]='mongodb+srv://varunraskar22:Shockwave07@cluster0.4tcbg1f.mongodb.net/todo'
+# application.config["SECRET_KEY"] = 'a8bbb89011cc826c2bccb6fc1f53d779c1e59814'
+# application.config["MONGO_URI"]='mongodb+srv://varunraskar22:Shockwave07@cluster0.4tcbg1f.mongodb.net/todo'
 
-mongodb_client = PyMongo(application)
-db = mongodb_client.db
+# mongodb_client = PyMongo(application)
+# db = mongodb_client.db
 
 from main import routes
--- main/routes.py
+++ main/routes.py
@@ -1,70 +1,72 @@
 
 
-from main import application,db
+from main import application #,db
 from flask import render_template,flash,request,redirect,url_for
 from .forms import TodoForm
 from datetime import datetime
 from bson import ObjectId
 
-@application.route("/")
-def get_todos():
-    todos = []
-    for todo in db.todos_flask.find().sort("date_created",-1):
-        todo["_id"] = str(todo["_id"])
-        todo["data_created"] = todo["date_created"].strftime("%b %d %Y %H:%M:%S")
-        todos.append(todo)
-    return render_template("views_todo.html",title="Layout page", todos=todos)
+# @application.route("/")
+# def get_todos():
+#     todos = []
+#     for todo in db.todos_flask.find().sort("date_created",-1):
+#         todo["_id"] = str(todo["_id"])
+#         todo["data_created"] = todo["date_created"].strftime("%b %d %Y %H:%M:%S")
+#         todos.append(todo)
+#     return render_template("views_todo.html",title="Layout page", todos=todos)
 
-@application.route("/add_todo", methods = ['POST', 'GET'])
-def add_todo():
-    if request.method == "POST":
-        form = TodoForm(request.form)
-        todo_name = form.name.data
-        todo_description = form.description.data
-        completed = form.completed.data
+# @application.route("/add_todo", methods = ['POST', 'GET'])
+# def add_todo():
+#     if request.method == "POST":
+#         form = TodoForm(request.form)
+#         todo_name = form.name.data
+#         todo_description = form.description.data
+#         completed = form.completed.data
 
-        db.todos_flask.insert_one({
-            "name": todo_name,
-            "description": todo_description,
-            "completed": completed,
-            "date_created": datetime.utcnow()
-        })
-        flash("Todo successfully added", "success")
-        return redirect("/")
-    else:
-        form = TodoForm()
-    return render_template("add_todo.html", form = form)
+#         db.todos_flask.insert_one({
+#             "name": todo_name,
+#             "description": todo_description,
+#             "completed": completed,
+#             "date_created": datetime.utcnow()
+#         })
+#         flash("Todo successfully added", "success")
+#         return redirect("/")
+#     else:
+#         form = TodoForm()
+#     return render_template("add_todo.html", form = form)
 
-@application.route("/update_todo/<id>", methods=["POST","GET"])
-def update_todo(id):
-    if request.method== "POST":
-        form = TodoForm(request.form)
-        todo_name = form.name.data
-        todo_description = form.description.data
-        completed = form.completed.data
+# @application.route("/update_todo/<id>", methods=["POST","GET"])
+# def update_todo(id):
+#     if request.method== "POST":
+#         form = TodoForm(request.form)
+#         todo_name = form.name.data
+#         todo_description = form.description.data
+#         completed = form.completed.data
 
-        db.todos_flask.find_one_and_update({"_id":ObjectId(id)}, 
-                                           {"$set":{"name":todo_name,
-                                                    "description": todo_description,
-                                                    "completed": completed,
-                                                    "date_created": datetime.utcnow()
-                                            }})
-        flash("Todo successfully updated", "success")
-        return redirect("/")
+#         db.todos_flask.find_one_and_update({"_id":ObjectId(id)}, 
+#                                            {"$set":{"name":todo_name,
+#                                                     "description": todo_description,
+#                                                     "completed": completed,
+#                                                     "date_created": datetime.utcnow()
+#                                             }})
+#         flash("Todo successfully updated", "success")
+#         return redirect("/")
     
-    else:
-        form = TodoForm()
-        todo = db.todos_flask.find_one_or_404({"_id":ObjectId(id)})
-        form.name.data = todo.get("name",None)
-        form.description.data = todo.get("description", None)
-        form.completed.data = todo.get("completed",None)
+#     else:
+#         form = TodoForm()
+#         todo = db.todos_flask.find_one_or_404({"_id":ObjectId(id)})
+#         form.name.data = todo.get("name",None)
+#         form.description.data = todo.get("description", None)
+#         form.completed.data = todo.get("completed",None)
 
-    return render_template("add_todo.html",form=form)
+#     return render_template("add_todo.html",form=form)
 
-@application.route("/delete_todo/<id>")
-def delete_todo(id):
-    db.todos_flask.find_one_and_delete({"_id":ObjectId(id)})
-    flash("Todo deleted successfully" , "success")
-    return redirect("/")
+# @application.route("/delete_todo/<id>")
+# def delete_todo(id):
+#     db.todos_flask.find_one_and_delete({"_id":ObjectId(id)})
+#     flash("Todo deleted successfully" , "success")
+#     return redirect("/")
 
-         
+@application.route("/")
+def home():
+    return render_template("u.html")
